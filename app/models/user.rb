3e4f50531d74112e99ba5dbfb7d7eb5d1d2fde10
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-09 15:00:54 +0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { maximum: 105 },
+                      format: { with: URI::MailTo::EMAIL_REGEXP }
 end
