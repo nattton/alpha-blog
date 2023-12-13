f2cbd36045310066da71bc960c296aa363b3ead8
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-14 00:33:59 +0700

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
                       uniqueness: { case_sensitive: false },
                       length: { maximum: 105 },
                       format: { with: URI::MailTo::EMAIL_REGEXP }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_154623) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_13_172201) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_154623) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
