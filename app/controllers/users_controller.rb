bdceadfe2a3575b6f7950889d677d49e8169705c
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-14 01:59:48 +0700

    Add users create action

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have successfully signup"
+      redirect_to articles_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
