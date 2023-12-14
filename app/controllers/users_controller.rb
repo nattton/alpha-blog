1ea71ccc28f935de902d93407172ffaa639f841d
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-14 08:48:05 +0700

    Add edit template and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,19 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[ show edit update destroy ]
+
   def new
     @user = User.new
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,7 +24,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to articles_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
+
   private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
