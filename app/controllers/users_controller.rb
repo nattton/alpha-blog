b0ba02d37a2ecdcaa7743dec5d3d2a9cbe96026a
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-16 13:40:27 +0700

    Restrict actions for users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :require_user, only: %i[ edit update ]
+  before_action :require_same_user, only: %i[ edit update destroy ]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,6 +39,10 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated articles successfully deleted"
+    redirect_to articles_path
   end
 
   private
@@ -46,5 +52,12 @@ class UsersController < ApplicationController
 
     def user_params
       params.require(:user).permit(:username, :email, :password)
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only edit your own account"
+        redirect_to @user
+      end
     end
 end
