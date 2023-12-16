9c8fa0779d69380d9bd7b5680ce63c3dc6a5a83b
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-16 13:54:22 +0700

    Add admin to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_13_172201) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_16_064059) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_13_172201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
