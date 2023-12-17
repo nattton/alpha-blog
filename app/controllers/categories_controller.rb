05d8f47efcb15adf5223b011a0628d4ef3e5342b
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-17 13:49:44 +0700

    Complete category index and show views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show ]
+  before_action :set_category, only: %i[ show edit update ]
   before_action :require_admin, except: %i[ index show ]
 
   def index
@@ -7,10 +7,23 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def new
     @category = Category.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update(category_params)
+      redirect_to @category
+      flash[:notice] = "Category was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def create
