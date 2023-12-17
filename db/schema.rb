06a0c7bbd4ed3cea95a83448347f0a536689ba9a
Nattapong Tonprasert <luciferultram@gmail.com>
2023-12-18 02:02:42 +0700

    Change database from sqlite to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2023_12_16_165656) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
     t.integer "category_id"
